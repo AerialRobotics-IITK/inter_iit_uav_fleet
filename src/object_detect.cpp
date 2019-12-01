297b4679f0eecbb4109490e1efd17470b5da9c9f
Pence <pencem@iitk.ac.in>
2019-12-01 23:29:30 +0530

    added cv_bridge

--- src/object_detect.cpp
+++ src/object_detect.cpp
@@ -1,12 +1,33 @@
 #include <ros/ros.h>
 #include <opencv2/opencv.hpp>
+#include <cv_bridge/cv_bridge.h>
+#include <sensor_msgs/Image.h>
 #include <opencv-3.3.1-dev/opencv2/opencv.hpp>
-#include </home/tanay/src/catkin_ws/src/inter_iit_uav_fleet/include/inter_iit_uav_fleet/outlier_filter.h>
+#include <inter_iit_uav_fleet/outlier_filter.h>
+
+cv::Mat src = cv::Mat::zeros(cv::Size(640, 480), CV_8UC3);
+void imageCallback(const sensor_msgs::Image msg)
+{
+    cv_bridge::CvImagePtr cv_ptr;
+    try
+    {
+        cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
+    }
+    catch (cv_bridge::Exception &e)
+    {
+        ROS_ERROR("cv_bridge exception: %s", e.what());
+        return;
+    }
+
+    src = cv_ptr->image;
+
+    return;
+}
 
 int main(int argc, char** argv)
 {
     ros::init(argc, argv, "object_detect");
-    ros::NodeHandle nh_private = ros::NodeHandle("~");
+    ros::NodeHandle nh_private = ros::NodeHandle();
 
     int i, j;
     int h_low, s_low, v_low, h_high, s_high, v_high;
@@ -17,26 +38,26 @@ int main(int argc, char** argv)
     nh_private.getParam("h_high", h_high);
     nh_private.getParam("s_high", s_high);
     nh_private.getParam("v_high", v_high);
-    
-    cv::Mat src, src_hsv, thresholded_hsv, thresholded_hsv1, drawing, drawing1;
 
-    std::vector<std::vector<cv::Point> > list_contours;
-    std::vector<std::vector<cv::Point> > list_corners;
-    std::vector<std::vector<int> > hull;
+    cv::Mat src_hsv, thresholded_hsv, thresholded_hsv1, drawing, drawing1;
+
+    std::vector<std::vector<cv::Point>> list_contours;
+    std::vector<std::vector<cv::Point>> list_corners;
+    std::vector<std::vector<int>> hull;
     std::vector<cv::Point> corners;
 
-    cv::VideoCapture cap;
-    cap.open(1);
+    ros::Subscriber image_sub = nh_private.subscribe<sensor_msgs::Image>("image", 30, imageCallback);
+    ros::Publisher marked_imgPub = nh_private.advertise<sensor_msgs::Image>("marked_image", 1);
+    ros::Rate loop_rate(10);
 
-    while(1)    
+    while (ros::ok())
     {
-        cap >> src;
-        // src = cv::imread("/home/tanay/src/catkin_ws/src/inter_iit_uav_fleet/etc/image_2.png");
+        ros::spinOnce();
         cv::GaussianBlur(src, src, cv::Size(3, 3), 0, 0);
         cv::cvtColor(src, src_hsv, CV_BGR2HSV);
         cv::inRange(src_hsv, cv::Scalar(h_low, s_low, v_low), cv::Scalar(h_high, s_high, v_high), thresholded_hsv);
-        
-        cv::morphologyEx(thresholded_hsv, thresholded_hsv1, CV_MOP_OPEN, getStructuringElement(cv::MORPH_RECT, cv::Size(3,3)), cv::Point(-1, -1), 1); 
+
+        cv::morphologyEx(thresholded_hsv, thresholded_hsv1, CV_MOP_OPEN, getStructuringElement(cv::MORPH_RECT, cv::Size(3, 3)), cv::Point(-1, -1), 1);
         cv::findContours(thresholded_hsv, list_contours, CV_RETR_LIST, CV_CHAIN_APPROX_NONE);
 
         drawing = cv::Mat::zeros(src.size(), CV_8UC1);
@@ -44,9 +65,9 @@ int main(int argc, char** argv)
 
         hull.resize(list_contours.size());
 
-        for(i=0; i<list_contours.size(); i++)
+        for (i = 0; i < list_contours.size(); i++)
         {
-            if(cv::contourArea(list_contours.at(i))>0.0025*src.rows*src.cols)
+            if (cv::contourArea(list_contours.at(i)) > 0.0025 * src.rows * src.cols)
             {
                 list_corners.clear();
                 corners.clear();
@@ -56,18 +77,21 @@ int main(int argc, char** argv)
                 outlier_filter(list_contours.at(i), hull.at(i), corners);
                 list_corners.push_back(corners);
 
-                if(!list_corners.at(0).empty() && list_corners.at(0).size()==4)
-                    cv::drawContours(drawing, list_corners, 0, cv::Scalar(255, 255, 255));
+                if (!list_corners.at(0).empty() && list_corners.at(0).size() == 4);
+                    cv::drawContours(src, list_corners, 0, cv::Scalar(255, 0, 0));
             }
         }
 
-        cv::imshow("In", src);
-        cv::imshow("contours", drawing1);
-        cv::imshow("Thresholded Image", thresholded_hsv);
-        cv::imshow("Thresholded Image1", thresholded_hsv1);
-        cv::imshow("Out", drawing);
-        if((char)cv::waitKey(30)=='q')
-            return 0;
+        // cv::imshow("In", src);
+        // cv::imshow("contours", drawing1);
+        // cv::imshow("Thresholded Image", thresholded_hsv);
+        // cv::imshow("Thresholded Image1", thresholded_hsv1);
+        // cv::imshow("Out", drawing);
+
+        sensor_msgs::ImagePtr marked_msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", src).toImageMsg();
+        marked_imgPub.publish(marked_msg);
+
+        loop_rate.sleep();
     } 
     return 0;
 }
