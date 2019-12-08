1e5af10f1577f00a4344a1bf34103771d37beefb
Kshitij1K <kabeer@iitk.ac.in>
2019-12-08 11:41:29 +0530

    Correct pose estimation

--- include/inter_iit_uav_fleet/pose.h
+++ include/inter_iit_uav_fleet/pose.h
@@ -5,16 +5,12 @@
 
 void findPose(const cv::Point& center, inter_iit_uav_fleet::Pose& box_pose)
 {
-    Eigen::Matrix3d scaleUp, quadToGlob2;
-    Eigen::Quaterniond quat2;
-    tf::Matrix3x3 quadToGlob;
-    tf::Quaternion quadToGlobquat;
-    
-    tfScalar roll,pitch,yaw;
+    Eigen::Matrix3d scaleUp, quadToGlob;
+    //Eigen::Quaterniond quadToGlob_eigen;
 
     tf::Quaternion q1(odom.pose.pose.orientation.x, odom.pose.pose.orientation.y, odom.pose.pose.orientation.z, odom.pose.pose.orientation.w);
     Eigen::Quaternionf quat = Eigen::Quaternionf(q1.w(), q1.x(), q1.y(), q1.z());
-    quadToGlob.setRotation(q1);
+    quadToGlob = quat.normalized().toRotationMatrix().inverse();
 
     // for (int i=0;i<3;i++){
     //     for (int j=0;j<3;j++){
@@ -22,13 +18,14 @@ void findPose(const cv::Point& center, inter_iit_uav_fleet::Pose& box_pose)
     //     }
     // }
 
-    quadToGlob.getRPY(roll,pitch,yaw);
-    yaw = -(CV_PI/180)*(heading.data<180 ? heading.data : heading.data-360);
-    quadToGlob.setRPY(roll,pitch,yaw);
-    quadToGlob.getRotation(quadToGlobquat);
+    //quadToGlob.getRPY(roll,pitch,yaw);
+    // yaw = -(CV_PI/180)*(heading.data<180 ? heading.data : heading.data-360);
+    // GlobtoLatLong.setRPY(0,0,yaw);
+    //quadToGlob.setRPY(roll,pitch,yaw);
+    //quadToGlob.getRotation(quadToLatLonquat);
     
-    tf::quaternionTFToEigen(quadToGlobquat,quat2);
-    quadToGlob2 = quat2.normalized().toRotationMatrix();
+    //tf::quaternionTFToEigen(quadToLatLonquat,quadToLatLonquat_eigen);
+    //quadToLatLon_eigen = quadToLatLonquat_eigen.normalized().toRotationMatrix();
 
     for (int i=0; i<3; i++)
     {
@@ -42,7 +39,7 @@ void findPose(const cv::Point& center, inter_iit_uav_fleet::Pose& box_pose)
     Eigen::Vector3d imgVec(center.x,center.y,1);
     Eigen::Vector3d quadCoord = (camToQuad*scaleUp*invCamMatrix*imgVec) + tCam;
 
-    Eigen::Vector3d globCoord = quadToGlob2*quadCoord;
+    Eigen::Vector3d globCoord = quadToGlob*quadCoord;
     GeographicLib::AzimuthalEquidistant obj;
     obj.Reverse(quad_GPS.latitude,quad_GPS.longitude,globCoord[0],globCoord[1],box_pose.position.x,box_pose.position.y);
     box_pose.position.z = 0;
